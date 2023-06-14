b5d9d96d7428a7e5a0d95243bf9625964adecedc
Lynn Thit Nyi Nyi <lnyinyi22@gmail.com>
2023-06-14 11:28:22 +0700

    preprocessing sample

--- Week 2/preprocessing-sample.py
+++ Week 2/preprocessing-sample.py
@@ -1,15 +1,36 @@
 import re
 
-sentence = "I have so much fun. Yahoo! lnyinyi22@gmail.com #computerscience https://oshinokoo.com/manga/oshi-no-ko-chapter-71/"
+sentence = "I have so much fun. Yahoo! u6411271@au.edu lNyinyi22@gmail.com #computerscience #IT https://oshinokoo.com/manga/oshi-no-ko-chapter-71/ https://oshinokoo.com/manga/oshi-no-ko-chapter-72/"
 
 def preproccsss_sentence(sentence):
+    emails = []
+    urls = []
+    hashtags = []
+    # extract URLS
+    urls.extend(re.findall(r"http\S+|www\S+|https\S+", sentence))
     sentence = re.sub(r"http\S+|www\S+|https\S+", "", sentence)
+    #extract email addresses from
+    emails.extend(re.findall(r"\S+@\S+", sentence))
     sentence = re.sub(r"\S+@\S+", "", sentence)
+    #extract hashtags
+    hashtags.extend(re.findall(r"#\w+", sentence))
     sentence = re.sub(r"#\w+", "", sentence)
+    #remove punctuations
     sentence = re.sub(r"[.!]", "", sentence)
+    #finalize
     words = sentence.split()
-    return words
+    return [words, emails, urls, hashtags, ]
 
-words = preproccsss_sentence(sentence)
+words, emails, urls, hashtags,  = preproccsss_sentence(sentence)
 
-print(words)
+with open("emails.txt", "w") as file:
+    for each in emails:
+      file.write(each + "\n")
+
+with open("urls.txt", "w") as file:
+    for each in urls:
+      file.write(each + "\n")
+
+with open("hashtags.txt", "w") as file:
+    for each in hashtags:
+      file.write(each + "\n")
