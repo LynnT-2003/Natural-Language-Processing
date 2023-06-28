46a18b774b3bb8dc401b24e5574e7043fea78244
Lynn Thit Nyi Nyi <lnyinyi22@gmail.com>
2023-06-28 10:47:34 +0700

    Intro to Sentiment Analysis.

---
+++ Week 4/sentiment-analysis.py
@@ -0,0 +1,18 @@
+from nltk.corpus import movie_reviews
+from collections import Counter
+from sklearn.model_selection import train_test_split
+
+print(len(movie_reviews.fileids()))
+print(movie_reviews.categories())
+print(movie_reviews.words()[:100])
+print(movie_reviews.fileids()[:10])
+
+documents = [(list(movie_reviews.words(fileid)), category) 
+            for category in movie_reviews.categories() 
+            for fileid in movie_reviews.fileids(category)]
+
+# print(documents[0])
+print(len(documents))
+
+distribution = Counter([label for (words, label) in documents])
+print(distribution)
